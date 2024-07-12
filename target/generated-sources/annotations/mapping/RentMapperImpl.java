7bfa5eedb6da919e211cd714f23eb5b02b606b6b
m.kuzmin <m.kuzmin@intabia.ru>
2024-07-12 10:51:04 +0700

    Полностью перешел на Spring JDBC.

---
+++ target/generated-sources/annotations/mapping/RentMapperImpl.java
@@ -0,0 +1,33 @@
+package mapping;
+
+import Service.Library;
+import Service.Rent;
+import dao.RentDAO;
+
+public class RentMapperImpl implements RentMapper {
+
+    @Override
+    public RentDAO toRentDAO(Rent rent) {
+        if ( rent == null ) {
+            return null;
+        }
+
+        RentDAO rentDAO = new RentDAO();
+
+        return rentDAO;
+    }
+
+    @Override
+    public Rent fromDAO(RentDAO rentDao) {
+        if ( rentDao == null ) {
+            return null;
+        }
+
+        Library library = null;
+
+        Rent rent = new Rent( library );
+
+        return rent;
+    }
+
+}
