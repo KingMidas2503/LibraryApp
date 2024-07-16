eeb8834c25c688893d1ef7b71e3a53246d67d576
m.kuzmin <m.kuzmin@intabia.ru>
2024-07-16 17:35:30 +0700

    Переделал CI код под мавен

--- src/main/java/config/LibraryProjectConfig.java
+++ src/main/java/config/LibraryProjectConfig.java
@@ -2,6 +2,7 @@ package config;
 
 import Service.Library;
 import Service.Reader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +14,13 @@ public class LibraryProjectConfig {
 
     @Bean
     @Scope("prototype")
-    public Library library(String title) {
+    public Library library(@Value("${library.title}") String title) {
         return new Library(title);
     }
+
     @Bean
     @Scope("prototype")
-    public Reader reader(String name) {
+    public Reader reader(@Value("${reader.name}") String name) {
         return new Reader(name);
     }
 
