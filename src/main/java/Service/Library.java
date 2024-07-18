8e08afe2b5ebf80b520b12f78eb37e63f59faf3a
m.kuzmin <m.kuzmin@intabia.ru>
2024-07-18 13:53:01 +0700

    Переделал CI код под мавен

--- src/main/java/Service/Library.java
+++ src/main/java/Service/Library.java
@@ -5,20 +5,38 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import jakarta.persistence.*;
 import lombok.Getter;
 
 
+@Entity
+@Table(name = "LIBRARIES")
 public class Library {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "LIBRARY_ID")
+    private List<Book> books = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "LIBRARY_ID")
+    List<Librarian> workers = new ArrayList<>();
+
+
 
     @Getter
     private String title;
 
     private int bookId;
     Rent rent;
-    List<Librarian> workers = new ArrayList<>();;
-    Map<String, Reader> readersRegistry = new HashMap<>();;
+    Map<String, Reader> readersRegistry = new HashMap<>();
     LibraryDAO libraryDAO = new LibraryDAO();
 
+    public Library() {}
 
 
     public Library(String title) {
