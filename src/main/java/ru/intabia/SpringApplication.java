5421412bbb420a0bc78ac9984cdc012d1b68829b
m.kuzmin <m.kuzmin@intabia.ru>
2024-08-05 14:08:46 +0700

    Проверяю работу stage deploy

--- src/main/java/ru/intabia/SpringApplication.java
+++ src/main/java/ru/intabia/SpringApplication.java
@@ -1,13 +1,29 @@
 package ru.intabia;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
-@SpringBootApplication
+/*@SpringBootApplication
 public class SpringApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(SpringApplication.class).run();
     }
+}*/
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
