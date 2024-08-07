7eb298dd94a93439fada1b4afc116bd371849950
KingMidas2503 <mihailkuzmin2003@mail.ru>
2024-08-07 17:21:56 +0700

    Failed to configure a DataSource: 'url' attribute is not specified and no embedded datasource could be configured.

--- src/main/java/ru/intabia/SpringApplication.java
+++ src/main/java/ru/intabia/SpringApplication.java
@@ -1,10 +1,11 @@
 package ru.intabia;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SpringApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/ru/intabia/config/DataSourceConfig.java
@@ -0,0 +1,26 @@
+package ru.intabia.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource(@Value("${spring.datasource.url}") String jdbcUrl,
+                                 @Value("${spring.datasource.username}") String username,
+                                 @Value("${spring.datasource.password}") String password) {
+        var config = new HikariConfig();
+        config.setJdbcUrl(jdbcUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName("org.postgresql.Driver");
+
+        return new HikariDataSource(config);
+    }
+}
